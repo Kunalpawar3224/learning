4838f53f20fd4c670a6441266352a048b3a7b073
kunal pawar <kpawar3224@gmail.com>
2022-11-29 12:04:14 +0530

    Added _name_=='_main_':

--- fibo.py
+++ fibo.py
@@ -1,11 +1,12 @@
 # Fibonacci numbers module
 
-def fib(n):    # write Fibonacci series up to n
+def fibo(n):    # write Fibonacci series up to n
     a, b = 0, 1
     while a < n:
         print(a, end=' ')
         a, b = b, a+b
     print()
-fib(2000)
 
-fib(3000)
+series=fibo(2000)
+if __name__=='_main_':
+ print(series)
