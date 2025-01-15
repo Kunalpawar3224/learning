6131d8d3205c8a1a8378ffc10ccd33d851d94753
Kunal Pawar <kpawar3224@gmail.com>
2025-01-15 10:10:20 +0530

    Added another logic of palindrome

--- dsa/palindrome.py
+++ dsa/palindrome.py
@@ -1,10 +1,22 @@
-str = 'JaVaJ'  
-strstr = str.casefold()  
+# str = 'JaVaJ'  
+# strstr = str.casefold()  
+# print("strstr = ", strstr)
+# # This string is reverse.  
+# rev = reversed(str)  
   
-# This string is reverse.  
-rev = reversed(str)  
-  
-if list(str) == list(rev):  
-   print("PALINDROME !")  
+# if list(str) == list(rev):  
+#    print("PALINDROME !")  
+# else:  
+#    print("NOT PALINDROME !")  
+
+num = 121
+temp = num  
+rev = 0  
+while(num > 0):  
+    dig = num % 10  
+    revrev = rev * 10 + dig  
+    numnum = num // 10  
+if(temp == rev):  
+    print("This value is a palindrome number!")  
 else:  
-   print("NOT PALINDROME !")  
+    print("This value is not a palindrome number!")  
