1296ceca2ba26bd2b9588b517ee1125e314e028d
Kunal Pawar <kpawar3224@gmail.com>
2025-09-07 14:18:07 +0530

    Added llm model

--- genai_learning/main.py
+++ genai_learning/main.py
@@ -1,2 +1,23 @@
+from groq import Groq
+api_key = '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'
+
+
 def chatbot(message):
-    return "connected"
+    client = Groq(api_key=api_key)
+    completion = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+        {
+            "role": "user",
+            "content": message
+        }
+        ],
+        temperature=1,
+        max_completion_tokens=1024,
+        top_p=1,
+        stream=False,
+        stop=None
+    )
+    return completion.choices[0].message.content
+# for chunk in completion:
+#     print(chunk.choices[0].delta.content or "", end="")
