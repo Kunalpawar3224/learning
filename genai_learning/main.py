711e1ae71370587c02deb1eae73d3f60a146ed57
Kunal Pawar <kpawar3224@gmail.com>
2026-01-06 09:48:25 +0530

    Add environment configuration and prompt logic

--- genai_learning/main.py
+++ genai_learning/main.py
@@ -1,12 +1,21 @@
+from fastapi import FastAPI
 from groq import Groq
+from pydantic import BaseModel
+import uvicorn
 
+from env import api_key, llama_model
+from prompt import prompt
 
 
 def chatbot(message):
     client = Groq(api_key=api_key)
     completion = client.chat.completions.create(
-        model="llama-3.1-8b-instant",
+        model= llama_model,
         messages=[
+        {
+            "role": "user",
+            "content": prompt
+        },
         {
             "role": "user",
             "content": message
@@ -21,3 +30,18 @@ def chatbot(message):
     return completion.choices[0].message.content
 # for chunk in completion:
 #     print(chunk.choices[0].delta.content or "", end="")
+
+
+
+# @app.post("/user-message") 
+# def respond(message:userMessage):
+#     response = chatbot(message.message)
+#     return{
+#         "result": response
+#     }
+
+
+# if __name__ == "__main__":
+#     uvicorn.run("app:app", port=8000, reload= True)
+
+
---
+++ genai_learning/prompt.py
@@ -0,0 +1,3 @@
+prompt = """
+        You are a chatbot who verifies that messages are spam or not.
+        """
